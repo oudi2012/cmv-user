614eed8ee17e5f4ed10b3d5459029fcc8f88bdc2
oudi2012 <wanghaikuo000@163.com>
2020-10-22 19:39:26 +0800

    init_02

---
+++ src/main/java/com/mycmv/user/CmvUserApplication.java
@@ -0,0 +1,45 @@
+package com.mycmv.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author a
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.mycmv"})
+@MapperScan("com.mycmv.user.mapper.user")
+public class CmvUserApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(CmvUserApplication.class);
+
+	public static void main(String[] args) throws UnknownHostException {
+		SpringApplication app = new SpringApplication(CmvUserApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		String protocol = "http";
+		if (env.getProperty("server.ssl.key-store") != null) {
+			protocol = "https";
+		}
+		log.info("\n----------------------------------------------------------\n\t" +
+						"Application '{}' is running! Access URLs:\n\t" +
+						"Local: \t\t{}://localhost:{}\n\t" +
+						"External: \t{}://{}:{}\n\t" +
+						"Profile(s): \t{}\n----------------------------------------------------------",
+				env.getProperty("spring.application.name"),
+				protocol,
+				env.getProperty("server.port"),
+				protocol,
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"),
+				env.getActiveProfiles());
+	}
+
+}
